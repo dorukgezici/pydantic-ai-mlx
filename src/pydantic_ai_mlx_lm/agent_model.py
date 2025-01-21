ed639f88abb2f181da24ac70ff741dd4aceb3a4f
Doruk Gezici <doruk@gezici.me>
2025-01-21 18:47:26 +0100

    Adding streaming support

--- src/pydantic_ai_mlx_lm/agent_model.py
+++ src/pydantic_ai_mlx_lm/agent_model.py
@@ -1,27 +1,22 @@
-# from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from itertools import chain
-from typing import Generator, Literal, overload
+from typing import AsyncIterable, AsyncIterator, Literal, overload
 
-from devtools import debug
-
-# from typing import AsyncIterator, Generator, Iterable, Literal, overload
 from mlx.nn import Module  # pyright: ignore[reportMissingTypeStubs]
 from mlx_lm.tokenizer_utils import TokenizerWrapper
 from mlx_lm.utils import GenerationResponse, generate, stream_generate  # pyright: ignore[reportUnknownVariableType]
-from pydantic_ai.messages import (
-    ModelMessage,
-    ModelResponse,
-    TextPart,
-)
-
-# from pydantic_ai.models import AgentModel, StreamedResponse
-from pydantic_ai.models import AgentModel
+from pydantic_ai import _utils
+from pydantic_ai.exceptions import UnexpectedModelBehavior
+from pydantic_ai.messages import ModelMessage, ModelResponse, TextPart
+from pydantic_ai.models import AgentModel, StreamedResponse
 from pydantic_ai.settings import ModelSettings
 from pydantic_ai.tools import ToolDefinition
 from pydantic_ai.usage import Usage
 
+from .generator import AsyncGeneratorWrapper
+from .response import MLXStreamedResponse
 from .utils import map_message
 
 
@@ -45,16 +40,14 @@ class MLXAgentModel(AgentModel):
         response = self._completions_create(messages, False, model_settings)
         return self._process_response(response), Usage()
 
-    # @asynccontextmanager
-    # async def request_stream(
-    #     self,
-    #     messages: list[ModelMessage],
-    #     model_settings: ModelSettings | None,
-    # ) -> AsyncIterator[StreamedResponse]:
-    #     """Make a streaming request to the model."""
+    @asynccontextmanager
+    async def request_stream(
+        self, messages: list[ModelMessage], model_settings: ModelSettings | None
+    ) -> AsyncIterator[StreamedResponse]:
+        """Make a streaming request to the model."""
 
-    #     response = self._completions_create(messages, True, model_settings)
-    #     yield await self._process_streamed_response(response)
+        response = self._completions_create(messages, True, model_settings)
+        yield await self._process_streamed_response(response)
 
     @overload
     def _completions_create(
@@ -65,16 +58,15 @@ class MLXAgentModel(AgentModel):
     @overload
     def _completions_create(
         self, messages: list[ModelMessage], stream: Literal[True], model_settings: ModelSettings | None
-    ) -> Generator[GenerationResponse, None, None]:
+    ) -> AsyncIterable[GenerationResponse]:
         pass
 
     def _completions_create(
         self, messages: list[ModelMessage], stream: bool, model_settings: ModelSettings | None
-    ) -> str | Generator[GenerationResponse, None, None]:
+    ) -> str | AsyncIterable[GenerationResponse]:
         """Standalone function to make it easier to override"""
 
         conversation = list(chain(*(map_message(m) for m in messages)))
-        debug(conversation)
 
         if not stream:
             return generate(
@@ -87,7 +79,7 @@ class MLXAgentModel(AgentModel):
                 max_tokens=model_settings.get("max_tokens", 1000) if model_settings else 1000,
             )
 
-        return stream_generate(
+        generator = stream_generate(
             model=self.model,
             tokenizer=self.tokenizer,
             prompt=self.tokenizer.apply_chat_template(  # pyright: ignore reportUnknownMemberType
@@ -96,6 +88,7 @@ class MLXAgentModel(AgentModel):
             ),
             max_tokens=model_settings.get("max_tokens", 1000) if model_settings else 1000,
         )
+        return AsyncGeneratorWrapper(generator)
 
     @staticmethod
     def _process_response(response: str) -> ModelResponse:
@@ -103,13 +96,14 @@ class MLXAgentModel(AgentModel):
 
         return ModelResponse(parts=[TextPart(content=response)], timestamp=datetime.now(timezone.utc))
 
-    # @staticmethod
-    # async def _process_streamed_response(response: Generator[GenerationResponse, None, None]) -> MLXStreamedResponse:
-    #     """Process a streamed response, and prepare a streaming response to return."""
+    @staticmethod
+    async def _process_streamed_response(response: AsyncIterable[GenerationResponse]) -> MLXStreamedResponse:
+        """Process a streamed response, and prepare a streaming response to return."""
 
-    #     peekable_response = _utils.PeekableAsyncStream(response)
-    #     first_chunk = await peekable_response.peek()
-    #     if isinstance(first_chunk, _utils.Unset):
-    #         raise UnexpectedModelBehavior("Streamed response ended without content or tool calls")
+        peekable_response = _utils.PeekableAsyncStream(response)
+        first_chunk = await peekable_response.peek()
 
-    #     return MLXStreamedResponse(peekable_response, datetime.fromtimestamp(first_chunk.created, tz=timezone.utc))
+        if isinstance(first_chunk, _utils.Unset):
+            raise UnexpectedModelBehavior("Streamed response ended without content or tool calls")
+
+        return MLXStreamedResponse(peekable_response, datetime.now(timezone.utc))
--- src/pydantic_ai_mlx_lm/response.py
+++ src/pydantic_ai_mlx_lm/response.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import AsyncIterable, AsyncIterator
 
-from openai.types import chat
+from mlx_lm.utils import GenerationResponse
 from pydantic_ai.messages import ModelResponseStreamEvent
 from pydantic_ai.models import StreamedResponse
 
@@ -11,30 +11,26 @@ from pydantic_ai.models import StreamedResponse
 class MLXStreamedResponse(StreamedResponse):
     """Implementation of `StreamedResponse` for MLX models."""
 
-    _response: AsyncIterable[chat.ChatCompletionChunk]
+    _response: AsyncIterable[GenerationResponse]
     _timestamp: datetime
 
     async def _get_event_iterator(self) -> AsyncIterator[ModelResponseStreamEvent]:
         async for chunk in self._response:
-            try:
-                choice = chunk.choices[0]
-            except IndexError:
-                continue
-
             # Handle the text part of the response
-            content = choice.delta.content
-            if content is not None:
+            content = chunk.text
+            if content:
                 yield self._parts_manager.handle_text_delta(vendor_part_id="content", content=content)
 
-            for dtc in choice.delta.tool_calls or []:
-                maybe_event = self._parts_manager.handle_tool_call_delta(
-                    vendor_part_id=dtc.index,
-                    tool_name=dtc.function and dtc.function.name,
-                    args=dtc.function and dtc.function.arguments,
-                    tool_call_id=dtc.id,
-                )
-                if maybe_event is not None:
-                    yield maybe_event
+            # TODO: Handle tool calls
+            # for dtc in choice.delta.tool_calls or []:
+            #     maybe_event = self._parts_manager.handle_tool_call_delta(
+            #         vendor_part_id=dtc.index,
+            #         tool_name=dtc.function and dtc.function.name,
+            #         args=dtc.function and dtc.function.arguments,
+            #         tool_call_id=dtc.id,
+            #     )
+            #     if maybe_event is not None:
+            #         yield maybe_event
 
     def timestamp(self) -> datetime:
         return self._timestamp
